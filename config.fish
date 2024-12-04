64763d5db85515f29ff87626070d79bf758eec36
JQiue <861947542@qq.com>
2024-12-04 19:43:46 +0800

    update linux.sh

--- config.fish
+++ config.fish
@@ -2,6 +2,12 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+alias sc 'systemctl status'
+alias sclist 'systemctl list-units --type=service'
+alias scstart 'systemctl start'
+alias scstop 'systemctl stop'
+alias screstart 'systemctl restart'
+alias scenable 'systemctl enable'
+alias scdisable 'systemctl disable'
+alias sclog 'journalctl -u'
+alias sclogclean 'journalctl --vacuum-time=1s -u'
--- linux.sh
+++ linux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 link_file() {
     local source=$1
-    local destination=\$2
+    local destination=$2
     # 如果目标文件存在
     if [ -f "$destination" ]; then
         # 创建备份
@@ -11,4 +11,5 @@ link_file() {
     ln -s "$source" "$destination"
 }
 
-link_file ./.gitconfig ~/.gitconfig
+link_file ~/dotfiles/.gitconfig ~/.gitconfig
+link_file ~/dotfiles/config.fish ~/.config/fish/config.fish
